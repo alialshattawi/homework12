988f4686d5084f5fd88297222a66bfd8fab8bc75
ali alshattawi <ali.alshattawi@gmail.com>
2017-04-26 20:20:08 +0000

    validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,15 @@
 class Doctor < ActiveRecord::Base
     has_many :patients
+   
+    validates_uniqueness_of :license_number
+   
+    validates_uniqueness_of :phone_number
+   
+    validates_format_of :phone_number, with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+   
+    validates_uniqueness_of :email
+   
+    validates_format_of :email, :with => /\A([^@\s]+)@((gmail)\.+com)\Z/i, :on => :create ,:message =>"must be @gmail.com"
+
 end
 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,24 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+  
+validates_presence_of :first_name, message: 'can not be blank'
+
+validates_presence_of :last_name, message: 'can not be blank'
+
+validates_uniqueness_of :first_name
+
+validates_uniqueness_of :last_name
+
+validates_uniqueness_of :phone
+
+validates_format_of :phone, with: /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+
+validates_uniqueness_of :email
+
+validates_format_of :email, :with => /\A([^@\s]+)@((gmail)\.+com)\Z/i, :on => :create ,:message =>"must be @gmail.com"
+
+
+
+
+
 end
